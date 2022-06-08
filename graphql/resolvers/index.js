0d1be26e99d598b953e9f3ccdbab88414db6257d
jhahn678 <jhahn678@yahoo.com>
2022-06-08 14:59:56 -0400

    Added new resolvers to root resolver

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,19 +1,27 @@
 const userResolvers = require('./user')
 const groupResolvers = require('./group')
 const messageResolvers = require('./message')
+const placeResolvers = require('./place')
+const catchResolvers = require('./catch')
 
 module.exports = {
     Query: {
         ...userResolvers.Query,
         ...groupResolvers.Query,
-        ...messageResolvers.Query
+        ...messageResolvers.Query,
+        ...placeResolvers.Query,
+        ...catchResolvers.Query
     },
     Mutation: {
         ...userResolvers.Mutation,
         ...groupResolvers.Mutation,
-        ...messageResolvers.Mutation
+        ...messageResolvers.Mutation,
+        ...placeResolvers.Mutation,
+        ...catchResolvers.Mutation
     },
     User: { ...userResolvers.User },
     Group: { ...groupResolvers.Group },
-    Message: { ...messageResolvers.Message }
+    Message: { ...messageResolvers.Message },
+    Place: { ...placeResolvers.Place },
+    Catch: { ...catchResolvers.Catch }
 }
